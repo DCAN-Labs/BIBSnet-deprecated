cf3e4dd65cf98fcb78264bed7abaf27431bff1ba
Timothy Hendrickson <hendr522@acn18.agate.msi.umn.edu>
2023-07-25 21:45:23 -0500

    added preBIBSnet and postBIBSnet functions into run.py

--- run.py
+++ run.py
@@ -27,6 +27,132 @@ def main():
 
     # Show user how long the pipeline took and end the pipeline here
     exit_with_time_info(start_time)
+    
+
+def run_preBIBSnet(j_args, logger):
+    """
+    :param j_args: Dictionary containing all args from parameter .JSON file
+    :param logger: logging.Logger object to show messages and raise warnings
+    :return: j_args, but with preBIBSnet working directory names added
+    """
+    completion_msg = "The anatomical images have been {} for use in BIBSnet"
+    preBIBSnet_paths = get_and_make_preBIBSnet_work_dirs(j_args)
+    sub_ses = get_subj_ID_and_session(j_args)
+
+    # If there are multiple T1ws/T2ws, then average them
+    create_anatomical_averages(preBIBSnet_paths["avg"], logger)  # TODO make averaging optional with later BIBSnet model?
+
+    # Crop T1w and T2w images
+    cropped = dict()
+    crop2full = dict()
+    for t in only_Ts_needed_for_bibsnet_model(j_args["ID"]):
+        cropped[t] = preBIBSnet_paths[f"crop_T{t}w"]
+        crop2full[t] = crop_image(preBIBSnet_paths["avg"][f"T{t}w_avg"],
+                                  cropped[t], j_args, logger)
+    logger.info(completion_msg.format("cropped"))
+
+    # Resize T1w and T2w images if running a BIBSnet model using T1w and T2w
+    # TODO Make ref_img an input parameter if someone wants a different reference image?
+    # TODO Pipeline should verify that reference_img files exist before running
+    reference_img = os.path.join(SCRIPT_DIR, "data", "MNI_templates",
+                                 "INFANT_MNI_T{}_1mm.nii.gz") 
+    id_mx = os.path.join(SCRIPT_DIR, "data", "identity_matrix.mat")
+    # TODO Resolution is hardcoded; infer it or get it from the command-line
+    resolution = "1"  
+    if j_args["ID"]["has_T1w"] and j_args["ID"]["has_T2w"]:
+        msg_xfm = "Arguments for {}ACPC image transformation:\n{}"
+
+        # Non-ACPC
+        regn_non_ACPC = register_preBIBSnet_imgs_non_ACPC(
+            cropped, preBIBSnet_paths["resized"], reference_img, 
+            id_mx, resolution, j_args, logger
+        )
+        if j_args["common"]["verbose"]:
+            logger.info(msg_xfm.format("non-", regn_non_ACPC["vars"]))
+
+        # ACPC
+        regn_ACPC = register_preBIBSnet_imgs_ACPC(
+            cropped, preBIBSnet_paths["resized"], regn_non_ACPC["vars"],
+            crop2full, preBIBSnet_paths["avg"], j_args, logger
+        )
+        if j_args["common"]["verbose"]:
+            logger.info(msg_xfm.format("", regn_ACPC["vars"]))
+
+        transformed_images = apply_final_prebibsnet_xfms(
+            regn_non_ACPC, regn_ACPC, preBIBSnet_paths["avg"], j_args, logger
+        )
+        logger.info(completion_msg.format("resized"))
+
+    # If running a T1w-only or T2w-only BIBSnet model, skip registration/resizing
+    else:
+        # Define variables and paths needed for the final (only) xfm needed
+        t1or2 = 1 if j_args["ID"]["has_T1w"] else 2
+        outdir = os.path.join(preBIBSnet_paths["resized"], "xfms")
+        os.makedirs(outdir, exist_ok=True)
+        out_img = get_preBIBS_final_img_fpath_T(t1or2, outdir, j_args["ID"])
+        crop2BIBS_mat = os.path.join(outdir,
+                                     "crop2BIBS_T{}w_only.mat".format(t1or2))
+        out_mat = os.path.join(outdir, "full_crop_T{}w_to_BIBS_template.mat"
+                                       .format(t1or2))
+
+        run_FSL_sh_script(  # Get xfm moving the T1 (or T2) into BIBS space
+            j_args, logger, "flirt", "-in", cropped[t1or2],
+            "-ref", reference_img.format(t1or2), "-applyisoxfm", resolution,
+            "-init", id_mx, # TODO Should this be a matrix that does a transformation?
+            "-omat", crop2BIBS_mat
+        )
+
+        # Invert crop2full to get full2crop
+        # TODO Move this to right after making crop2full, then delete the 
+        #      duplicated functionality in align_ACPC_1_image
+        full2crop = os.path.join(
+            os.path.dirname(preBIBSnet_paths["avg"][f"T{t}w_avg"]),
+            f"full2crop_T{t}w_only.mat"
+        )
+        run_FSL_sh_script(j_args, logger, "convert_xfm", "-inverse",
+                          crop2full[t], "-omat", full2crop) 
+
+        # - Concatenate crop .mat to out_mat (in that order) and apply the
+        #   concatenated .mat to the averaged image as the output
+        # - Treat that concatenated output .mat as the output to pass
+        #   along to postBIBSnet, and the image output to BIBSnet
+        run_FSL_sh_script(  # Combine ACPC-alignment with robustFOV output
+            j_args, logger, "convert_xfm", "-omat", out_mat,
+            "-concat", full2crop, crop2BIBS_mat
+        )
+        run_FSL_sh_script(  # Apply concat xfm to crop and move into BIBS space
+            j_args, logger, "applywarp", "--rel", "--interp=spline",
+            "-i", preBIBSnet_paths["avg"][f"T{t}w_avg"],
+            "-r", reference_img.format(t1or2),
+            "--premat=" + out_mat, "-o", out_img
+        )
+        transformed_images = {f"T{t1or2}w": out_img,
+                              f"T{t1or2}w_crop2BIBS_mat": out_mat}
+
+    # TODO Copy this whole block to postBIBSnet, so it copies everything it needs first
+    # Copy preBIBSnet outputs into BIBSnet input dir
+    for t in only_Ts_needed_for_bibsnet_model(j_args["ID"]): 
+        # Copy image files
+        out_nii_fpath = j_args["optimal_resized"][f"T{t}w"]
+        os.makedirs(os.path.dirname(out_nii_fpath), exist_ok=True)
+        if j_args["common"]["overwrite"]:  # TODO Should --overwrite delete old image file(s)?
+            os.remove(out_nii_fpath)
+        if not os.path.exists(out_nii_fpath): 
+            shutil.copy2(transformed_images[f"T{t}w"], out_nii_fpath)
+
+        # Copy .mat into postbibsnet dir with the same name regardless of which
+        # is chosen, so postBIBSnet can use the correct/chosen .mat file
+        concat_mat = transformed_images[f"T{t}w_crop2BIBS_mat"]
+        out_mat_fpath = os.path.join(  # TODO Pass this in (or out) from the beginning so we don't have to build the path twice (once here and once in postBIBSnet)
+            j_args["optional_out_dirs"]["postbibsnet"],
+            *sub_ses, "preBIBSnet_" + os.path.basename(concat_mat)
+        )
+        if not os.path.exists(out_mat_fpath):
+            shutil.copy2(concat_mat, out_mat_fpath)
+            if j_args["common"]["verbose"]:
+                logger.info(f"Copying {concat_mat} to {out_mat_fpath}")
+    logger.info("PreBIBSnet has completed")
+    return j_args
 
 
 def run_nnUNet_predict(cli_args):
@@ -47,6 +173,92 @@ def run_nnUNet_predict(cli_args):
                  "at the path below, check their filenames and visually "
                  "inspect them if needed.\n{}\n\n"
                  .format(cli_args["output"], cli_args["input"]))
+
+def run_postBIBSnet(j_args, logger):
+    """
+    :param j_args: Dictionary containing all args from parameter .JSON file
+    :param logger: logging.Logger object to show messages and raise warnings
+    :return: j_args, unchanged
+    """
+    sub_ses = get_subj_ID_and_session(j_args)
+
+    # Template selection values
+    age_months = j_args["ID"]["age_months"]
+    logger.info("Age of participant: {} months".format(age_months))
+
+    # Get template closest to age
+    tmpl_age = get_template_age_closest_to(
+        age_months, os.path.join(SCRIPT_DIR, "data", "chirality_masks")
+    )
+    if j_args["common"]["verbose"]:
+        logger.info("Closest template-age is {} months".format(tmpl_age))
+
+    # For left/right registration, use T1 for T1-only and T2 for T2-only, but
+    # for T1-and-T2 combined use T2 for <22 months otherwise T1 (img quality)
+    if j_args["ID"]["has_T1w"] and j_args["ID"]["has_T2w"]:
+        t1or2 = 2 if int(age_months) < 22 else 1  # NOTE 22 cutoff might change
+    elif j_args["ID"]["has_T1w"]:
+        t1or2 = 1
+    else:  # if j_args["ID"]["has_T2w"]:
+        t1or2 = 2
+
+    # Run left/right registration script and chirality correction
+    left_right_mask_nifti_fpath = run_left_right_registration(
+        sub_ses, tmpl_age, t1or2, j_args, logger
+    )
+    logger.info("Left/right image registration completed")
+
+    # Dilate the L/R mask and feed the dilated mask into chirality correction
+    if j_args["common"]["verbose"]:
+        logger.info("Now dilating left/right mask")
+    dilated_LRmask_fpath = dilate_LR_mask(
+        os.path.join(j_args["optional_out_dirs"]["postbibsnet"], *sub_ses),
+        left_right_mask_nifti_fpath
+    )
+    logger.info("Finished dilating left/right segmentation mask")
+    nifti_file_paths, chiral_out_dir, xfm_ref_img_dict = run_correct_chirality(dilated_LRmask_fpath,
+                                                      j_args, logger)
+    for t in only_Ts_needed_for_bibsnet_model(j_args["ID"]):
+        nii_outfpath = reverse_regn_revert_to_native(
+            nifti_file_paths, chiral_out_dir, xfm_ref_img_dict[t], t, j_args, logger
+        )
+        
+        logger.info("The BIBSnet segmentation has had its chirality checked and "
+                    "registered if needed. Now making aseg-derived mask.")
+
+        # TODO Skip mask creation if outputs already exist and not j_args[common][overwrite]
+        aseg_mask = make_asegderived_mask(j_args, chiral_out_dir, t, nii_outfpath)  # NOTE Mask must be in native T1 space too
+        logger.info(f"A mask of the BIBSnet T{t} segmentation has been produced")
+
+        # Make nibabies input dirs
+        bibsnet_derivs_dir = os.path.join(j_args["optional_out_dirs"]["derivatives"], 
+                                    "bibsnet")
+        derivs_dir = os.path.join(bibsnet_derivs_dir, *sub_ses, "anat")
+        os.makedirs(derivs_dir, exist_ok=True)
+        copy_to_derivatives_dir(nii_outfpath, derivs_dir, sub_ses, t, "aseg_dseg")
+        copy_to_derivatives_dir(aseg_mask, derivs_dir, sub_ses, t, "brain_mask")
+        input_path = os.path.join(j_args["common"]["bids_dir"],
+                                               *sub_ses, "anat",
+                                               f"*T{t}w.nii.gz")
+        reference_path = glob(input_path)[0]
+        generate_sidecar_json(sub_ses, reference_path, derivs_dir, t, "aseg_dseg")
+        generate_sidecar_json(sub_ses, reference_path, derivs_dir, t, "brain_mask")
+
+    # Copy dataset_description.json into bibsnet_derivs_dir directory for use in nibabies
+    new_data_desc_json = os.path.join(bibsnet_derivs_dir, "dataset_description.json")
+    if j_args["common"]["overwrite"]:
+        os.remove(new_data_desc_json)
+    if not os.path.exists(new_data_desc_json):
+        shutil.copy2(os.path.join(SCRIPT_DIR, "data",
+                                  "dataset_description.json"), new_data_desc_json)
+    if j_args["common"]["work_dir"] == os.path.join("/", "tmp", "cabinet"):
+        shutil.rmtree(j_args["common"]["work_dir"])
+        logger.info("Working Directory removed at {}."
+                    "To keep the working directory in the future,"
+                    "set a directory with the --work-dir flag.\n"
+                    .format(j_args['common']['work_dir']))
+    logger.info("PostBIBSnet has completed.")
+    return j_args
 
 
 def get_cli_args():
